5e46ae966f4afae04a29c3812afc01d2b5de5557
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-21 21:11:34 -0300

    feat: configurando envs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const config = app.get<ConfigService>(ConfigService);
+  const port = config.get<number>('PORT');
+  const appUrl = config.get<string>('LOCAL_URL');
+
+  await app.listen(port, (): void => {
+    logger.log(`Server is running on ${appUrl}:${port}`);
+  });
 }
 bootstrap();
