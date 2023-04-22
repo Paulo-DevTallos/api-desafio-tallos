bbce0dd8d421f0d67d2007f11f9e1804956435d5
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-22 13:27:39 -0300

    feat: configurando apontamento do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { UserModule } from './user/user.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    MongooseModule.forRoot(process.env.DB_STRING_CONNECTION),
     UserModule,
   ],
   controllers: [],
