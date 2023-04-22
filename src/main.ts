a20e8a2eff7388561b348788c9d286ca28a373e3
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-22 13:38:00 -0300

    feat: configurando swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap(): Promise<void> {
@@ -9,6 +10,15 @@ async function bootstrap(): Promise<void> {
   const config = app.get<ConfigService>(ConfigService);
   const port = config.get<number>('PORT');
   const appUrl = config.get<string>('LOCAL_URL');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('API gerenciador de usuários')
+    .setDescription('sistema de gestão')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, (): void => {
     logger.log(`Server is running on ${appUrl}:${port}`);
