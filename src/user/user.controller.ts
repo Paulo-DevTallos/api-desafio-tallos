21a4cc494da46c7ed47fdaeaf2b5e9b34f0e0931
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-22 19:55:33 -0300

    feat: implementando dependencia de criação de usuarios

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDto } from 'src/user/dto/create.user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('create')
+  async createUser(@Body() data: UserDto): Promise<UserDto> {
+    return await this.userService.create(data);
+  }
+
+  @Get()
+  async getAllUsers(): Promise<UserDto[]> {
+    return this.userService.getAll();
+  }
+}
